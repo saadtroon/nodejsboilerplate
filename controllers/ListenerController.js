431f65068e7a38487519de5456443cc2ca39d02b
saadtroon <saad@troontechnologies.com>
2022-06-12 00:38:42 +0500

    duplicate farms handled

--- controllers/ListenerController.js
+++ controllers/ListenerController.js
@@ -107,6 +107,21 @@ const contract =  new web3.eth.Contract((abi), '0x005152D60516D761112A284ec623FB
               })
             .on('error', (e) => {
             console.log('--SomeEvent--Error',e);
+            })
+            .on('end', (e) => {
+                console.log('--SomeEvent--End',e);
+                })
+            .on('close', (e) => {
+            console.log('--SomeEvent--close',e);
+            })
+            .on('timeout', (e) => {
+                console.log('--SomeEvent--timeout',e);
+            })
+            .on('exit', (e) => {
+                console.log('--SomeEvent--exit',e);
+            })
+            .on('ready', (e) => {
+                console.log('--SomeEvent--Ready',e);
             });
 
         console.log("end");
@@ -139,15 +154,14 @@ const contract =  new web3.eth.Contract((abi), '0x005152D60516D761112A284ec623FB
         if (error) {
             return error;
         } 
-        }).on('data', function (data) {
+        }).on('data',async function (data) {
             var type;
             console.log(data.returnValues);
             type = determineType(data.returnValues[1])
             var query = {categoryName: type.toLowerCase()};
             var nextUpdatedTimestamp = Date.now();
             nextUpdatedTimestamp = nextUpdatedTimestamp + (20 * 86400000);
-
-           
+            var res =await Farm.findOne( {farmId: data.returnValues[2]} )           
 
         CategoryDetailModel.find(query).then(category => {
             
@@ -173,7 +187,10 @@ const contract =  new web3.eth.Contract((abi), '0x005152D60516D761112A284ec623FB
             });
 
         counter = category[0].counterNFT
-
+        if (res !== null ) {
+            console.log("duplicate",farm.farmId);
+            return;
+        } else {
                 //Save book.assignedNFT
                 farm.save(function (err) {
                     if (err) { console.log("error:",err); }
@@ -189,6 +206,7 @@ const contract =  new web3.eth.Contract((abi), '0x005152D60516D761112A284ec623FB
                         }
                     )
                 });
+            }
             });
         });           
 
