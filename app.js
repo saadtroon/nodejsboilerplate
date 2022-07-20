01fa74a90b5db08a7e7f6754f6ce19a1f97170be
saadtroon <saad@troontechnologies.com>
2022-07-20 23:37:04 +0500

    db name update

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ var cors = require("cors");
 // DB connection
 var MONGODB_URL = process.env.MONGODB_URL;
 var mongoose = require("mongoose");
-mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true,
+	useFindAndModify: false,
+	useCreateIndex: true,
+	dbName: 'shoefyDB', // IMPORTANT TO HAVE IT HERE AND NOT IN CONNECTION STRING
+}).then(() => {
 	//don't show the log when it is test
 	if(process.env.NODE_ENV !== "test") {
 		console.log("Connected to %s", MONGODB_URL);
