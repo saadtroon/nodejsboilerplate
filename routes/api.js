67a3d7e1714a8f8413628ce6ccabb0cc82a905fe
usmanfazil <usmanework99@gmail.com>
2022-09-21 03:11:46 +0500

    book controller removed

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,9 @@
 var express = require("express");
 var authRouter = require("./auth");
-var bookRouter = require("./book");
 
 var app = express();
 
 app.use("/auth/", authRouter);
-app.use("/book/", bookRouter);
+
 
 module.exports = app;
